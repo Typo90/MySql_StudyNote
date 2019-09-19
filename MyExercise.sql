ba43b08fc2345abc7ec27dfc1457a0de06d267ad
Typo90 <491507328@qq.com>
2019-09-19 16:49:35 +0800

    MySqlStudyNote

--- MyExercise.sql
+++ MyExercise.sql
@@ -344,4 +344,44 @@
 --     from employees
 -- ); 	
 
+#============================================================================
+#============================================================================
+
+
+#test_lesson10 数据处理
+#1. 运行以下脚本创建表 my_employees Create table my_employees(  Id int(10),  First_name varchar(10),  Last_name varchar(10),  Userid varchar(10),  Salary double(10,2) ) create table users(  id int,  userid varchar(10),  department_id int ) 
+Create table my_employees( 
+	Id int(10),  
+    First_name varchar(10),  
+    Last_name varchar(10),  
+    Userid varchar(10),  
+    Salary double(10,2) 
+); 
+create table users(  
+	id int,  userid 
+    varchar(10),  
+    department_id int 
+) ;
+ 
+#2. 显示表 my_employees 的结构 
+desc my_employees;
+#3. 向 my_employees 表中插入下列数据 
+#ID FIRST_NAME LAST_NAME USERID SALARY 1 patel Ralph Rpatel 895 2 Dancs Betty Bdancs 860 3 Biri Ben Bbiri 1100 4 Newman Chad Cnewman 750 5 Ropeburn Audrey Aropebur 1550  
+insert into my_employees(ID ,FIRST_NAME,LAST_NAME,USERID,SALARY)
+values (1,'patel' ,'Ralph' ,'Rpatel', 895)
+    
+#4.  向 users 表中插入数据
+ 
+ 
+ 
+ 
+ 
+#5. 将 3 号员工的 last_name 修改为“drelxer” 
+#6. 将所有工资少于 900 的员工的工资修改为 1000 
+#7. 将 userid 为 Bbiri 的 user 表和 my_employees 表的记录全部删除 
+#8. 删除所有数据 
+#9. 检查所作的修正 
+#10. 清空表 my_employees 
+ 
+ 
  
